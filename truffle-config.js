6eb5d44ca5fe296f9749f92e17ee1c91f0aa2fd3
Anni Piragauta <aapiragautau@unal.edu.co>
2020-04-08 15:15:01 -0500

    Adding celo network for test network Baklava Testnet

--- truffle-config.js
+++ truffle-config.js
@@ -52,6 +52,11 @@ module.exports = {
       port: 8545,            // Standard Ethereum port (default: none)
       network_id: "*",       // Any network (default: none)
      },
+    celo: {
+      host: "127.0.0.1",
+      port: 8545,
+      network_id: 40120,
+    },
 
     // Another network with more advanced options...
     // advanced: {

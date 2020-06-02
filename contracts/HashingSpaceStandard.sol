4d9adfdb1de06f713279b6a6997ed60044be5a9f
Mesi Rendon <amrendonsa@unal.edu.co>
2020-06-01 20:38:51 -0500

    Finishing hashing space

--- contracts/HashingSpaceStandard.sol
+++ contracts/HashingSpaceStandard.sol
@@ -3,9 +3,17 @@ pragma solidity >= 0.5.0 < 0.7.0;
 contract HashingSpaceStandard {
   bytes32 public ipfsHash;
   string public name;
+  bytes32[100] hs;
 
   constructor(bytes32 _ipfsHash, string memory _name) public {
     ipfsHash = _ipfsHash;
     name = _name;
+    for(uint i = 0; i < 100; i++) {
+      hs[i] = keccak256(abi.encodePacked(ipfsHash, i));
+    }
+  }
+
+  function validate(bytes32 _test, uint _idx) public view returns (bool){
+    return keccak256(abi.encodePacked(_test)) == keccak256(abi.encodePacked(hs[_idx]));
   }
 }
--- test/02-HashingSpace.js
+++ test/02-HashingSpace.js
@@ -1,4 +1,4 @@
-const HashingSpace = artifacts.require('HashingSpace');
+const HashingSpace = artifacts.require('HashingSpaceStandard');
 
 const chai = require('chai');
 const chaiAsPromised = require('chai-as-promised');
@@ -7,22 +7,43 @@ chai.use(chaiAsPromised);
 
 const { expect } = chai;
 
+const ipfsBytes32 = '0x318787a718972a977081bdae6d8adec7694eb99b0d9f8c83fc070d4db23a0287';
+
 contract('Hashing space', (accounts) => {
   const [
     owner,
   ] = accounts;
   describe('Initialization', () => {
-    it('should fail if no constructor parameters are passed', () => {
+    it('should create a new contract upon ipfs hash and name', () => {
       const HS = new web3.eth.Contract(HashingSpace.abi);
       const deployHS = HS.deploy({
         data: HashingSpace.bytecode,
-        arguments: [],
+        arguments: [ ipfsBytes32, 'Hashing Space 1' ],
       });
       return deployHS.estimateGas({from: owner})
         .then(gas => deployHS.send({from: owner, gas}))
-        .then(result => {
-          expect(result._address).to.match(/0x[a-fA-F0-9]{40}/);
+        .then(instance => instance.methods.ipfsHash().call())
+        .then(ipfshash => {
+          expect(ipfshash).to.eq(ipfsBytes32);
         });
+    });
+  });
+  describe('Operational', () => {
+    let hub;
+    beforeEach(async() => {
+      const HS = new web3.eth.Contract(HashingSpace.abi);
+      const deployHS = HS.deploy({
+        data: HashingSpace.bytecode,
+        arguments: [ ipfsBytes32, 'Hashing Space 1' ],
+      });
+      const deployHSGas = await deployHS.estimateGas({ from: owner });
+      hub = await deployHS.send({ from: owner, gas: deployHSGas });
+    });
+    it('should validate a hashed that was generated correctly', () => {
+      return hub.methods.validate(web3.utils.soliditySha3(ipfsBytes32, 69), 69).call()
+        .then(answer => {
+          expect(answer).to.be.true;
+        })
     });
   });
 });
